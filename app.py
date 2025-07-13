92c9b7b6cf8a11393fc224ce579b89e6fa7fddfa
biharichin <biharibhita@gmail.com>
2025-07-13 20:39:55 +0530

    debug

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def submit_doctor():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
 # For Render.com hosting
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)
